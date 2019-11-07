eb5096948cb299e193aa85b0044425f83885d649
Robin James Kerrison <rjk1994@gmail.com>
2019-11-07 14:10:24 +0000

    Adding some extra cases for Harry and his bear

--- definition.py
+++ definition.py
@@ -20,15 +20,16 @@ def show_sense_vectors(tokens, words):
       print(a_len, a, x.definition())
 
 def the_whole_thing(phrase, words):
-  print(phrase)
-  print(words)
-
   tokens = nltk.word_tokenize(phrase)
 
+  print(r'{:->10}{:-<90}'.format('', phrase))
   print('Determined senses for ', words)
   determine_sense(tokens, words)
 
   show_sense_vectors(tokens, words)
 
 the_whole_thing('Harry stood on the bank of the river. He could see a bear catching fish.', ['bear', 'bank'])
+the_whole_thing('Harry stood on the bank of the river from which he could see a bear catching fish.', ['bear', 'bank'])
+the_whole_thing('As he walked along the river, Harry stopped to gaze at the other bank, where a bear was quietly looking for fish.', ['bear', 'bank'])
+the_whole_thing('Harry saw a brown bear on the grassy bank.', ['bear', 'bank'])
 the_whole_thing('Harry worked at the bank during the bear market.', ['bear', 'bank'])
